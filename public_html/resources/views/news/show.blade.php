8decc8b920ec6a3b6e1fb75e96a7f624482d1a5b
Samuel Aiala Ferreira <samuca@samuca.com>
2018-12-19 13:08:06 -0200

    - Inclusão de botão para voltar, quando estiver lendo uma notícia - Correção da rota quando estiver vendo um detalhe de uma notícia - Inclusão dos arquivos de log no .gitignore

--- public_html/resources/views/news/show.blade.php
+++ public_html/resources/views/news/show.blade.php
@@ -4,6 +4,12 @@
 
 @section('content')
 
+    <div class="row text-center">
+        <div class="col-md-12">
+            <a href="/" class="btn btn-primary">Voltar</a>
+        </div>
+    </div>
+    
     <h3>{{ $news->title }}</h3>
     
     <p class="text-justify">{!! nl2br($news->description) !!}</p>
--- public_html/routes/web.php
+++ public_html/routes/web.php
@@ -17,3 +17,4 @@ Route::get('/', function() {
 
 Route::get('/news', 'NewsController@index');
 Route::get('/refresh-feed', 'FeedController@index');
+Route::get('/news/{id}/{slug}', 'NewsController@show');
